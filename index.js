a6cf5aeee60f255858dee3c2b0d481454251b99f
Natalie Halawani <hala1224@gmail.com>
2018-12-21 01:34:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,14 +8,15 @@
 //   repoList += '</ul>';
 //   document.getElementById('repositories').innerHTML = repoList;
 // }
-function showRepositories() {
-  var repos = JSON.parse(this.responseText);
-  console.log(repos);
-  const repoList = `<ul>${repos
-    .map(r => '<li>' + r.name + '</li>')
-    .join('')}</ul>`;
-  document.getElementById('repositories').innerHTML = repoList;
-}
+// function showRepositories() {
+//   var repos = JSON.parse(this.responseText);
+//   console.log(repos);
+//   const repoList = `<ul>${repos
+//     .map(r => '<li>' + r.name + '</li>')
+//     .join('')}</ul>`;
+//   document.getElementById('repositories').innerHTML = repoList;
+// }
+
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.addEventListener('load', showRepositories);
